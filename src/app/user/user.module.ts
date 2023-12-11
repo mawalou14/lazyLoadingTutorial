b84f72f1368ac9c93547b371db10ada1627ff50d
mawalou14 <awaloum213@gmail.com>
2023-12-11 01:27:11 +0100

    create user list

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
