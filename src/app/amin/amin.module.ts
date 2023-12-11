5ffd2eea898103ef4ed639d56034c9362d75fd1a
mawalou14 <awaloum213@gmail.com>
2023-12-11 01:08:33 +0100

    create dmin module routing

---
+++ src/app/amin/amin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AminRoutingModule } from './amin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AminRoutingModule
+  ]
+})
+export class AminModule { }
