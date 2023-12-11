da6fdf78e8c9f8304964dddf876722f5b92ea209
mawalou14 <awaloum213@gmail.com>
2023-12-11 01:32:45 +0100

    add user lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
       .then(mod => mod.AminModule)
   },
   {
-    path: 'admin', loadChildren: () => import('./amin/amin.module')
-      .then(mod => mod.AminModule)
+    path: 'user', loadChildren: () => import('./user/user.module')
+      .then(mod => mod.UserModule)
   }
 ];
 
