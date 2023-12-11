23025f6c4bc5250825afa0f2a7119e2081627da3
mawalou14 <awaloum213@gmail.com>
2023-12-11 01:13:50 +0100

    add list component path

--- src/app/amin/amin-routing.module.ts
+++ src/app/amin/amin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../admin/login/login.component';
+import { ListComponent } from '../admin/list/list.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ListComponent },
 ];
 
 @NgModule({
