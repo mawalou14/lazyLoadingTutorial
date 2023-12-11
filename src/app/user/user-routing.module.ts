8645ba218ecef9f4bc887bb46dc578400457b638
mawalou14 <awaloum213@gmail.com>
2023-12-11 01:30:28 +0100

    change login import

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
